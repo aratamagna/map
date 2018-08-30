bccda90587c57e1750649be728789e123493390c
kanra <aratamagna@gmail.com>
2018-08-30 11:44:25 -0300

    init

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class App extends React.Component {
+  render() {
+    return (
+       <MapView
+         style={styles.map}
+         initialRegion={{
+           latitude: 37.78825,
+           longitude: -122.4324,
+           latitudeDelta: 0.015,
+           longitudeDelta: 0.0121,
+         }}
+       />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+},
+map: {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+},
+});
